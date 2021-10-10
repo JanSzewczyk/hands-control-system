a4e98776c9dac07a163f2ded55e11bacba774db7
JanSzewczyk <jan.szewczyk1997@gmail.com>
2021-10-10 19:56:09 +0200

    Add images

---
+++ utils/HandUtils.py
@@ -0,0 +1,9 @@
+from typing import List
+from models import Hand
+
+
+def prepare_hand_data(hand: Hand) -> List[int]:
+    print(hand.border_box)
+    print(hand.landmarks)
+
+    return []
---
+++ utils/__init__.py
@@ -0,0 +1,1 @@
+from utils.HandUtils import *
