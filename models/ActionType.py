58267711b3399d5a7050d10deeef79b5b7687efa
JanSzewczyk <jan.szewczyk1997@gmail.com>
2021-10-19 20:17:23 +0200

    Create dataset

--- models/ActionType.py
+++ models/ActionType.py
@@ -1,9 +1,10 @@
 from enum import Enum
 
 
+# TODO change options/names
 class ActionType(Enum):
-    CLICK = 0
-    RESET = 1
+    NEUTRAL = 0
+    CLICK = 1
     GRAB = 2
     GO_BACK = 3
     GO_FORWARD = 4
